aa53035f958c7a7d7e1c0529c02670be02280c8f
Andy Nichols <andy.nichols@qt.io>
2018-06-06 15:10:32 +0200

    Fix spelling of gauge in qml import

--- src/imports/outgauge/qoutgaugeplugin.cpp
+++ src/imports/outgauge/qoutgaugeplugin.cpp
@@ -6,6 +6,6 @@
 void QOutGaugePlugin::registerTypes(const char *uri)
 {
     // @uri io.qad.OutGauge
-    qmlRegisterType<QOutGauge>(uri, 1, 0, "OutGuage");
+    qmlRegisterType<QOutGauge>(uri, 1, 0, "OutGauge");
 }
 
